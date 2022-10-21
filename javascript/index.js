bc0ff925dadfe99834529d49fe4e37bb027f629f
Kevinborras <65748900+ryomagetter@users.noreply.github.com>
2022-10-21 17:35:10 -0400

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,48 @@
+document.write("Mensaje enviado desde archivo Javascript");
+document.write("<br>"); 
+document.write("<hr>"); //Linea
+document.write("Minecraft"); 
+
+var nombre;
+nombre = "Juan Carlos";
+let apellido = "Medina"; //Se deja usar en procedure, functions, no es global, depende del alcance interno
+edad = 18;
+ventas = 1234.99;
+_bandera = true;
+dato = null;
+fecha = '2022-10-21';
+
+// fecha date = new fecha(); instancia
+
+//document.write(nombre);
+//document.write(apellido);
+
+//concatenacion
+document.write(nombre + ' ' + apellido);
+
+alert('La venta del dia es: ' + ventas);
+
+console.log('La fecha del dia: ' + fecha); // para saber si el c    odigo esta funcionando o no
+
+var name = prompt ("Ingrese su nombre","");
+console.log(name);
+
+let mensaje = confirm("Estas seguro de Eliminar el registro");
+//O es true o es false
+alert(mensaje);
+
+/*
++ - * / % 
+
+< > <= >= 
+
+= asignacion
+== comparacion
+=== identico
+! Negacion
+!= Diferente
+!== Cualquier
+
+&&
+||áéíóú└┬┼┴┴┬┬─┼┬
+*/
